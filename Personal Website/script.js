c4053cf2f89d63e466e4c559f385ad005aa7e45a
jamaal <palacebikes@gmail.com>
2023-03-30 20:12:07 -0700

    added new styling and new script for js

--- Personal Website/script.js
+++ Personal Website/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+const firstNum = 5 + 5;
+const secondNum = 80 / 2;
+const thirdNum = 3 * 13;
+
+//creating alert box
+alert(message + firstNum + " - " + secondNum + " - " + thirdNum);
