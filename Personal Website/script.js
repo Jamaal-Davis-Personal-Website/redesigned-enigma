93ace81ffc6478e17538aec3415461fbd7df51ae
jamaal <palacebikes@gmail.com>
2023-03-30 20:40:23 -0700

    i am done w this

--- Personal Website/script.js
+++ Personal Website/script.js
@@ -1,6 +1,6 @@
 const message = "You have received this message because you have been chosen to open an important page. "
 const firstWord = "Hi,"
 const secondWord = "Welcome,";
-const thirdWord = "Greetings!";
+const thirdWord = "Greetings to my first personal page!";
 
-alert(message + " "firstWord + " " + secondWord+ " " + thirdWord);
+alert(message + " " + firstWord + " " + secondWord+ " " + thirdWord);
